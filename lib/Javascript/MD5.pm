6fdec901d823d39161b07caa5301e893c32c8b96
Ron Savage <ron@savage.net.au>
2004-03-06 01:51:52 +0000

    Import of RSAVAGE/Javascript-MD5-1.00 from CPAN.
    gitpan-cpan-distribution: Javascript-MD5
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         RSAVAGE/Javascript-MD5-1.00.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

---
+++ lib/Javascript/MD5.pm
@@ -0,0 +1,397 @@
+package Javascript::MD5;
+
+# Name:
+#	Javascript::MD5.
+#
+# Documentation:
+#	POD-style documentation is at the end. Extract it with pod2html.*.
+#
+# Reference:
+#	Object Oriented Perl
+#	Damian Conway
+#	Manning
+#	1-884777-79-1
+#	P 114
+#
+# Note:
+#	o Tab = 4 spaces || die.
+#
+# Author:
+#	Ron Savage <ron@savage.net.au>
+#	Home page: http://savage.net.au/index.html
+#
+# Licence:
+#	Australian copyright (c) 2004 Ron Savage.
+#
+#	All Programs of mine are 'OSI Certified Open Source Software';
+#	you can redistribute them and/or modify them under the terms of
+#	The Artistic License, a copy of which is available at:
+#	http://www.opensource.org/licenses/index.html
+
+use strict;
+use warnings;
+no warnings 'redefine';
+
+require 5.005_62;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Javascript::MD5 ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+
+);
+our $VERSION = '1.00';
+
+# -----------------------------------------------
+
+# Preloaded methods go here.
+
+# -----------------------------------------------
+
+# Encapsulated class data.
+
+{
+	my(%_attr_data) =
+	(	# Alphabetical order.
+	);
+
+	sub _default_for
+	{
+		my($self, $attr_name) = @_;
+
+		$_attr_data{$attr_name};
+	}
+
+	sub _standard_keys
+	{
+		keys %_attr_data;
+	}
+
+}	# End of encapsulated class data.
+
+# -----------------------------------------------
+
+sub javascript
+{
+	my($self, $field_name) = @_;
+	$field_name ||= 'password';
+
+	return <<EOS;
+/*
+   script: $/myadmin/yahoo.js
+ posted by Tom St Dennis
+    keywords: crypt hash challenge login yahoo
+ * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
+ * Digest Algorithm, as defined in RFC 1321.
+ * Copyright (C) Paul Johnston 1999 - 2000.
+ * Updated by Greg Holt 2000 - 2001.
+ * See http://pajhome.org.uk/site/legal.html for details.
+ * Patches by Ron Savage 2004-03-05:
+ * o Uncalled functions removed
+ * o " changed to '
+ * o Hardcoded CGI form field name 'password' now a parameter, $field_name
+ * Original code: http://4.11.205.206/yahoo.zip.
+ */
+
+/*
+ * Convert a 32-bit number to a hex string with ls-byte first
+ */
+var hex_chr = '0123456789abcdef';
+function RetMD5()
+{
+	document.forms[0].$field_name.value = MD5(document.forms[0].$field_name.value);
+	return true;
+}
+function rhex(num)
+{
+  str = '';
+  for(j = 0; j <= 3; j++)
+    str += hex_chr.charAt((num >> (j * 8 + 4)) & 0x0F) +
+           hex_chr.charAt((num >> (j * 8)) & 0x0F);
+  return str;
+}
+
+/*
+ * Convert a string to a sequence of 16-word blocks, stored as an
+array.
+ * Append padding bits and the length, as described in the MD5
+standard.
+ */
+function str2blks_MD5(str)
+{
+  nblk = ((str.length + 8) >> 6) + 1;
+  blks = new Array(nblk * 16);
+  for(i = 0; i < nblk * 16; i++) blks[i] = 0;
+  for(i = 0; i < str.length; i++)
+    blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
+  blks[i >> 2] |= 0x80 << ((i % 4) * 8);
+  blks[nblk * 16 - 2] = str.length * 8;
+  return blks;
+}
+
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations
+internally
+ * to work around bugs in some JS interpreters.
+ */
+function add(x, y)
+{
+  var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  return (msw << 16) | (lsw & 0xFFFF);
+}
+
+/*
+ * Bitwise rotate a 32-bit number to the left
+ */
+function rol(num, cnt)
+{
+  return (num << cnt) | (num >>> (32 - cnt));
+}
+
+/*
+ * These functions implement the basic operation for each round of the
+ * algorithm.
+ */
+function cmn(q, a, b, x, s, t)
+{
+  return add(rol(add(add(a, q), add(x, t)), s), b);
+}
+function ff(a, b, c, d, x, s, t)
+{
+  return cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+function gg(a, b, c, d, x, s, t)
+{
+  return cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+function hh(a, b, c, d, x, s, t)
+{
+  return cmn(b ^ c ^ d, a, b, x, s, t);
+}
+function ii(a, b, c, d, x, s, t)
+{
+  return cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+/*
+ * Take a string and return the hex representation of its MD5.
+ */
+function MD5(str)
+{
+  x = str2blks_MD5(str);
+  var a =  1732584193;
+  var b = -271733879;
+  var c = -1732584194;
+  var d =  271733878;
+
+  for(i = 0; i < x.length; i += 16)
+  {
+    var olda = a;
+    var oldb = b;
+    var oldc = c;
+    var oldd = d;
+
+    a = ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+    d = ff(d, a, b, c, x[i+ 1], 12, -389564586);
+    c = ff(c, d, a, b, x[i+ 2], 17,  606105819);
+    b = ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+    a = ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+    d = ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+    c = ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+    b = ff(b, c, d, a, x[i+ 7], 22, -45705983);
+    a = ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+    d = ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+    c = ff(c, d, a, b, x[i+10], 17, -42063);
+    b = ff(b, c, d, a, x[i+11], 22, -1990404162);
+    a = ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+    d = ff(d, a, b, c, x[i+13], 12, -40341101);
+    c = ff(c, d, a, b, x[i+14], 17, -1502002290);
+    b = ff(b, c, d, a, x[i+15], 22,  1236535329);
+
+    a = gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+    d = gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+    c = gg(c, d, a, b, x[i+11], 14,  643717713);
+    b = gg(b, c, d, a, x[i+ 0], 20, -373897302);
+    a = gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+    d = gg(d, a, b, c, x[i+10], 9 ,  38016083);
+    c = gg(c, d, a, b, x[i+15], 14, -660478335);
+    b = gg(b, c, d, a, x[i+ 4], 20, -405537848);
+    a = gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+    d = gg(d, a, b, c, x[i+14], 9 , -1019803690);
+    c = gg(c, d, a, b, x[i+ 3], 14, -187363961);
+    b = gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+    a = gg(a, b, c, d, x[i+13], 5 , -1444681467);
+    d = gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+    c = gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+    b = gg(b, c, d, a, x[i+12], 20, -1926607734);
+
+    a = hh(a, b, c, d, x[i+ 5], 4 , -378558);
+    d = hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+    c = hh(c, d, a, b, x[i+11], 16,  1839030562);
+    b = hh(b, c, d, a, x[i+14], 23, -35309556);
+    a = hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+    d = hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+    c = hh(c, d, a, b, x[i+ 7], 16, -155497632);
+    b = hh(b, c, d, a, x[i+10], 23, -1094730640);
+    a = hh(a, b, c, d, x[i+13], 4 ,  681279174);
+    d = hh(d, a, b, c, x[i+ 0], 11, -358537222);
+    c = hh(c, d, a, b, x[i+ 3], 16, -722521979);
+    b = hh(b, c, d, a, x[i+ 6], 23,  76029189);
+    a = hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+    d = hh(d, a, b, c, x[i+12], 11, -421815835);
+    c = hh(c, d, a, b, x[i+15], 16,  530742520);
+    b = hh(b, c, d, a, x[i+ 2], 23, -995338651);
+
+    a = ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+    d = ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+    c = ii(c, d, a, b, x[i+14], 15, -1416354905);
+    b = ii(b, c, d, a, x[i+ 5], 21, -57434055);
+    a = ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+    d = ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+    c = ii(c, d, a, b, x[i+10], 15, -1051523);
+    b = ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+    a = ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+    d = ii(d, a, b, c, x[i+15], 10, -30611744);
+    c = ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+    b = ii(b, c, d, a, x[i+13], 21,  1309151649);
+    a = ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+    d = ii(d, a, b, c, x[i+11], 10, -1120210379);
+    c = ii(c, d, a, b, x[i+ 2], 15,  718787259);
+    b = ii(b, c, d, a, x[i+ 9], 21, -343485551);
+
+    a = add(a, olda);
+    b = add(b, oldb);
+    c = add(c, oldc);
+    d = add(d, oldd);
+  }
+  return rhex(a) + rhex(b) + rhex(c) + rhex(d);
+}
+EOS
+
+}	# End of javascript.
+
+# -----------------------------------------------
+
+sub new
+{
+	my($caller, %arg)	= @_;
+	my($caller_is_obj)	= ref($caller);
+	my($class)			= $caller_is_obj || $caller;
+	my($self)			= bless({}, $class);
+
+	for my $attr_name ($self -> _standard_keys() )
+	{
+		my($arg_name) = $attr_name =~ /^_(.*)/;
+
+		if (exists($arg{$arg_name}) )
+		{
+			$$self{$attr_name} = $arg{$arg_name};
+		}
+		elsif ($caller_is_obj)
+		{
+			$$self{$attr_name} = $$caller{$attr_name};
+		}
+		else
+		{
+			$$self{$attr_name} = $self -> _default_for($attr_name);
+		}
+	}
+
+	$self;
+
+}	# End of new.
+
+# -----------------------------------------------
+
+1;
+
+__END__
+
+=head1 NAME
+
+C<Javascript::MD5> - Calculate the MD5 digest of a CGI form field
+
+=head1 Synopsis
+
+This is a complete, runnable, tested program.
+
+	#!/usr/bin/perl
+
+	use strict;
+	use warnings;
+
+	use CGI;
+	use Javascript::MD5;
+
+	# ------------------
+
+	my($q)	= CGI -> new();
+	my($p)	= $q -> param('password') || '';
+	my($js)	= Javascript::MD5 -> new();
+
+	print $q -> header(),
+      $q -> start_html({script => $js -> javascript(), title => 'Javascript::MD5'}),
+      $q -> h1({align => 'center'}, 'Javascript::MD5'),
+      "Previous value: $p",
+      $q -> br(),
+      $q -> start_form({action => $q -> url(), name => 'md5'}),
+      'Username: ',
+      $q -> textfield({name => 'username', size => 50}),
+      $q -> br(),
+      'Password: ',
+      $q -> password_field({name => 'password', size => 50}),
+      $q -> br(),
+      $q -> submit({onClick => 'return RetMD5()'}),
+      $q -> end_form(),
+      $q -> end_html();
+
+=head1 Description
+
+C<Javascript::MD5> is a pure Perl module.
+
+This module allows you to convert user input, eg a password, into the MD5 digest of that
+input.
+
+This means the password itself need never be transmitted across the network - only the MD5
+digest need be transmitted.
+
+=head1 Distributions
+
+This module is available both as a Unix-style distro (*.tgz) and an
+ActiveState-style distro (*.ppd). The latter is shipped in a *.zip file.
+
+See http://savage.net.au/Perl-modules.html for details.
+
+See http://savage.net.au/Perl-modules/html/installing-a-module.html for
+help on unpacking and installing each type of distro.
+
+=head1 Constructor and initialization
+
+new(...) returns a C<CGI::Session::Manager> object.
+
+This is the class's contructor.
+
+Usage: CGI::Session::Manager -> new().
+
+C<new()> does not take any parameters.
+
+=head1 Method: javascript()
+
+Returns a block of Javascript which you must output as part of your HTML page.
+
+See the Synopsis for one way to do this.
