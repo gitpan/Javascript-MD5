c5d6bda99bb6b7d5d25d76e7203da5bfc0e7cd7c
Ron Savage <ron@savage.net.au>
2004-04-28 09:30:57 +0000

    Import of RSAVAGE/Javascript-MD5-1.03 from CPAN.
    gitpan-cpan-distribution: Javascript-MD5
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         RSAVAGE/Javascript-MD5-1.03.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/Javascript/MD5.pm
+++ lib/Javascript/MD5.pm
@@ -54,7 +54,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 
 );
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 # -----------------------------------------------
 
@@ -92,63 +92,209 @@ sub javascript
 
 	return <<EOS;
 /*
-   script: $/myadmin/yahoo.js
- posted by Tom St Dennis
-    keywords: crypt hash challenge login yahoo
  * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
  * Digest Algorithm, as defined in RFC 1321.
- * Copyright (C) Paul Johnston 1999 - 2000.
- * Updated by Greg Holt 2000 - 2001.
- * See http://pajhome.org.uk/site/legal.html for details.
- * Patches by Ron Savage 2004-03-05:
- * o Uncalled functions removed
- * o " changed to '
- * o Hardcoded CGI form field name 'password' now a parameter, $field_name
- * Original code: http://4.11.205.206/yahoo.zip.
+ * Version 2.1 Copyright (C) Paul Johnston 1999 - 2002.
+ * Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
+ * Distributed under the BSD License
+ * See http://pajhome.org.uk/crypt/md5 for more info.
+ * Patch by Ron Savage 2004-04-27:
+ * o Add the 3 functions str2hex_md5, str2b64_md5 and str2str_md5().
+ *	Note: In V 1.02 of this module, the first function was called
+ *	RetMD5 because that's what the Yahoo! programmers called it.
  */
 
-/*
- * Convert a 32-bit number to a hex string with ls-byte first
- */
-var hex_chr = '0123456789abcdef';
-function RetMD5()
+function str2hex_md5()
 {
-	document.forms[0].$field_name.value = MD5(document.forms[0].$field_name.value);
+	document.forms[0].$field_name.value = hex_md5(document.forms[0].$field_name.value);
 	return true;
 }
-function rhex(num)
+
+function str2b64_md5()
 {
-  str = '';
-  for(j = 0; j <= 3; j++)
-    str += hex_chr.charAt((num >> (j * 8 + 4)) & 0x0F) +
-           hex_chr.charAt((num >> (j * 8)) & 0x0F);
-  return str;
+	document.forms[0].$field_name.value = b64_md5(document.forms[0].$field_name.value);
+	return true;
+}
+
+function str2str_md5()
+{
+	document.forms[0].$field_name.value = str_md5(document.forms[0].$field_name.value);
+	return true;
 }
 
 /*
- * Convert a string to a sequence of 16-word blocks, stored as an
-array.
- * Append padding bits and the length, as described in the MD5
-standard.
+ * Configurable variables. You may need to tweak these to be compatible with
+ * the server-side, but the defaults work in most cases.
  */
-function str2blks_MD5(str)
+var hexcase = 0;  /* hex output format. 0 - lowercase; 1 - uppercase        */
+var b64pad  = ""; /* base-64 pad character. "=" for strict RFC compliance   */
+var chrsz   = 8;  /* bits per input character. 8 - ASCII; 16 - Unicode      */
+
+/*
+ * These are the functions you'll usually want to call
+ * They take string arguments and return either hex or base-64 encoded strings
+ */
+function hex_md5(s){ return binl2hex(core_md5(str2binl(s), s.length * chrsz));}
+function b64_md5(s){ return binl2b64(core_md5(str2binl(s), s.length * chrsz));}
+function str_md5(s){ return binl2str(core_md5(str2binl(s), s.length * chrsz));}
+function hex_hmac_md5(key, data) { return binl2hex(core_hmac_md5(key, data)); }
+function b64_hmac_md5(key, data) { return binl2b64(core_hmac_md5(key, data)); }
+function str_hmac_md5(key, data) { return binl2str(core_hmac_md5(key, data)); }
+
+/*
+ * Perform a simple self-test to see if the VM is working
+ */
+function md5_vm_test()
 {
-  nblk = ((str.length + 8) >> 6) + 1;
-  blks = new Array(nblk * 16);
-  for(i = 0; i < nblk * 16; i++) blks[i] = 0;
-  for(i = 0; i < str.length; i++)
-    blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
-  blks[i >> 2] |= 0x80 << ((i % 4) * 8);
-  blks[nblk * 16 - 2] = str.length * 8;
-  return blks;
+  return hex_md5("abc") == "900150983cd24fb0d6963f7d28e17f72";
 }
 
 /*
- * Add integers, wrapping at 2^32. This uses 16-bit operations
-internally
+ * Calculate the MD5 of an array of little-endian words, and a bit length
+ */
+function core_md5(x, len)
+{
+  /* append padding */
+  x[len >> 5] |= 0x80 << ((len) % 32);
+  x[(((len + 64) >>> 9) << 4) + 14] = len;
+
+  var a =  1732584193;
+  var b = -271733879;
+  var c = -1732584194;
+  var d =  271733878;
+
+  for(var i = 0; i < x.length; i += 16)
+  {
+    var olda = a;
+    var oldb = b;
+    var oldc = c;
+    var oldd = d;
+
+    a = md5_ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+    d = md5_ff(d, a, b, c, x[i+ 1], 12, -389564586);
+    c = md5_ff(c, d, a, b, x[i+ 2], 17,  606105819);
+    b = md5_ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+    a = md5_ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+    d = md5_ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+    c = md5_ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+    b = md5_ff(b, c, d, a, x[i+ 7], 22, -45705983);
+    a = md5_ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+    d = md5_ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+    c = md5_ff(c, d, a, b, x[i+10], 17, -42063);
+    b = md5_ff(b, c, d, a, x[i+11], 22, -1990404162);
+    a = md5_ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+    d = md5_ff(d, a, b, c, x[i+13], 12, -40341101);
+    c = md5_ff(c, d, a, b, x[i+14], 17, -1502002290);
+    b = md5_ff(b, c, d, a, x[i+15], 22,  1236535329);
+
+    a = md5_gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+    d = md5_gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+    c = md5_gg(c, d, a, b, x[i+11], 14,  643717713);
+    b = md5_gg(b, c, d, a, x[i+ 0], 20, -373897302);
+    a = md5_gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+    d = md5_gg(d, a, b, c, x[i+10], 9 ,  38016083);
+    c = md5_gg(c, d, a, b, x[i+15], 14, -660478335);
+    b = md5_gg(b, c, d, a, x[i+ 4], 20, -405537848);
+    a = md5_gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+    d = md5_gg(d, a, b, c, x[i+14], 9 , -1019803690);
+    c = md5_gg(c, d, a, b, x[i+ 3], 14, -187363961);
+    b = md5_gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+    a = md5_gg(a, b, c, d, x[i+13], 5 , -1444681467);
+    d = md5_gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+    c = md5_gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+    b = md5_gg(b, c, d, a, x[i+12], 20, -1926607734);
+
+    a = md5_hh(a, b, c, d, x[i+ 5], 4 , -378558);
+    d = md5_hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+    c = md5_hh(c, d, a, b, x[i+11], 16,  1839030562);
+    b = md5_hh(b, c, d, a, x[i+14], 23, -35309556);
+    a = md5_hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+    d = md5_hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+    c = md5_hh(c, d, a, b, x[i+ 7], 16, -155497632);
+    b = md5_hh(b, c, d, a, x[i+10], 23, -1094730640);
+    a = md5_hh(a, b, c, d, x[i+13], 4 ,  681279174);
+    d = md5_hh(d, a, b, c, x[i+ 0], 11, -358537222);
+    c = md5_hh(c, d, a, b, x[i+ 3], 16, -722521979);
+    b = md5_hh(b, c, d, a, x[i+ 6], 23,  76029189);
+    a = md5_hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+    d = md5_hh(d, a, b, c, x[i+12], 11, -421815835);
+    c = md5_hh(c, d, a, b, x[i+15], 16,  530742520);
+    b = md5_hh(b, c, d, a, x[i+ 2], 23, -995338651);
+
+    a = md5_ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+    d = md5_ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+    c = md5_ii(c, d, a, b, x[i+14], 15, -1416354905);
+    b = md5_ii(b, c, d, a, x[i+ 5], 21, -57434055);
+    a = md5_ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+    d = md5_ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+    c = md5_ii(c, d, a, b, x[i+10], 15, -1051523);
+    b = md5_ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+    a = md5_ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+    d = md5_ii(d, a, b, c, x[i+15], 10, -30611744);
+    c = md5_ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+    b = md5_ii(b, c, d, a, x[i+13], 21,  1309151649);
+    a = md5_ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+    d = md5_ii(d, a, b, c, x[i+11], 10, -1120210379);
+    c = md5_ii(c, d, a, b, x[i+ 2], 15,  718787259);
+    b = md5_ii(b, c, d, a, x[i+ 9], 21, -343485551);
+
+    a = safe_add(a, olda);
+    b = safe_add(b, oldb);
+    c = safe_add(c, oldc);
+    d = safe_add(d, oldd);
+  }
+  return Array(a, b, c, d);
+
+}
+
+/*
+ * These functions implement the four basic operations the algorithm uses.
+ */
+function md5_cmn(q, a, b, x, s, t)
+{
+  return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s),b);
+}
+function md5_ff(a, b, c, d, x, s, t)
+{
+  return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+function md5_gg(a, b, c, d, x, s, t)
+{
+  return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+function md5_hh(a, b, c, d, x, s, t)
+{
+  return md5_cmn(b ^ c ^ d, a, b, x, s, t);
+}
+function md5_ii(a, b, c, d, x, s, t)
+{
+  return md5_cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+/*
+ * Calculate the HMAC-MD5, of a key and some data
+ */
+function core_hmac_md5(key, data)
+{
+  var bkey = str2binl(key);
+  if(bkey.length > 16) bkey = core_md5(bkey, key.length * chrsz);
+
+  var ipad = Array(16), opad = Array(16);
+  for(var i = 0; i < 16; i++)
+  {
+    ipad[i] = bkey[i] ^ 0x36363636;
+    opad[i] = bkey[i] ^ 0x5C5C5C5C;
+  }
+
+  var hash = core_md5(ipad.concat(str2binl(data)), 512 + data.length * chrsz);
+  return core_md5(opad.concat(hash), 512 + 128);
+}
+
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations internally
  * to work around bugs in some JS interpreters.
  */
-function add(x, y)
+function safe_add(x, y)
 {
   var lsw = (x & 0xFFFF) + (y & 0xFFFF);
   var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
@@ -156,130 +302,72 @@ function add(x, y)
 }
 
 /*
- * Bitwise rotate a 32-bit number to the left
+ * Bitwise rotate a 32-bit number to the left.
  */
-function rol(num, cnt)
+function bit_rol(num, cnt)
 {
   return (num << cnt) | (num >>> (32 - cnt));
 }
 
 /*
- * These functions implement the basic operation for each round of the
- * algorithm.
+ * Convert a string to an array of little-endian words
+ * If chrsz is ASCII, characters >255 have their hi-byte silently ignored.
  */
-function cmn(q, a, b, x, s, t)
+function str2binl(str)
 {
-  return add(rol(add(add(a, q), add(x, t)), s), b);
-}
-function ff(a, b, c, d, x, s, t)
-{
-  return cmn((b & c) | ((~b) & d), a, b, x, s, t);
-}
-function gg(a, b, c, d, x, s, t)
-{
-  return cmn((b & d) | (c & (~d)), a, b, x, s, t);
-}
-function hh(a, b, c, d, x, s, t)
-{
-  return cmn(b ^ c ^ d, a, b, x, s, t);
-}
-function ii(a, b, c, d, x, s, t)
-{
-  return cmn(c ^ (b | (~d)), a, b, x, s, t);
+  var bin = Array();
+  var mask = (1 << chrsz) - 1;
+  for(var i = 0; i < str.length * chrsz; i += chrsz)
+    bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (i%32);
+  return bin;
 }
 
 /*
- * Take a string and return the hex representation of its MD5.
+ * Convert an array of little-endian words to a string
  */
-function MD5(str)
+function binl2str(bin)
 {
-  x = str2blks_MD5(str);
-  var a =  1732584193;
-  var b = -271733879;
-  var c = -1732584194;
-  var d =  271733878;
+  var str = "";
+  var mask = (1 << chrsz) - 1;
+  for(var i = 0; i < bin.length * 32; i += chrsz)
+    str += String.fromCharCode((bin[i>>5] >>> (i % 32)) & mask);
+  return str;
+}
 
-  for(i = 0; i < x.length; i += 16)
+/*
+ * Convert an array of little-endian words to a hex string.
+ */
+function binl2hex(binarray)
+{
+  var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+  var str = "";
+  for(var i = 0; i < binarray.length * 4; i++)
   {
-    var olda = a;
-    var oldb = b;
-    var oldc = c;
-    var oldd = d;
-
-    a = ff(a, b, c, d, x[i+ 0], 7 , -680876936);
-    d = ff(d, a, b, c, x[i+ 1], 12, -389564586);
-    c = ff(c, d, a, b, x[i+ 2], 17,  606105819);
-    b = ff(b, c, d, a, x[i+ 3], 22, -1044525330);
-    a = ff(a, b, c, d, x[i+ 4], 7 , -176418897);
-    d = ff(d, a, b, c, x[i+ 5], 12,  1200080426);
-    c = ff(c, d, a, b, x[i+ 6], 17, -1473231341);
-    b = ff(b, c, d, a, x[i+ 7], 22, -45705983);
-    a = ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
-    d = ff(d, a, b, c, x[i+ 9], 12, -1958414417);
-    c = ff(c, d, a, b, x[i+10], 17, -42063);
-    b = ff(b, c, d, a, x[i+11], 22, -1990404162);
-    a = ff(a, b, c, d, x[i+12], 7 ,  1804603682);
-    d = ff(d, a, b, c, x[i+13], 12, -40341101);
-    c = ff(c, d, a, b, x[i+14], 17, -1502002290);
-    b = ff(b, c, d, a, x[i+15], 22,  1236535329);
-
-    a = gg(a, b, c, d, x[i+ 1], 5 , -165796510);
-    d = gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
-    c = gg(c, d, a, b, x[i+11], 14,  643717713);
-    b = gg(b, c, d, a, x[i+ 0], 20, -373897302);
-    a = gg(a, b, c, d, x[i+ 5], 5 , -701558691);
-    d = gg(d, a, b, c, x[i+10], 9 ,  38016083);
-    c = gg(c, d, a, b, x[i+15], 14, -660478335);
-    b = gg(b, c, d, a, x[i+ 4], 20, -405537848);
-    a = gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
-    d = gg(d, a, b, c, x[i+14], 9 , -1019803690);
-    c = gg(c, d, a, b, x[i+ 3], 14, -187363961);
-    b = gg(b, c, d, a, x[i+ 8], 20,  1163531501);
-    a = gg(a, b, c, d, x[i+13], 5 , -1444681467);
-    d = gg(d, a, b, c, x[i+ 2], 9 , -51403784);
-    c = gg(c, d, a, b, x[i+ 7], 14,  1735328473);
-    b = gg(b, c, d, a, x[i+12], 20, -1926607734);
-
-    a = hh(a, b, c, d, x[i+ 5], 4 , -378558);
-    d = hh(d, a, b, c, x[i+ 8], 11, -2022574463);
-    c = hh(c, d, a, b, x[i+11], 16,  1839030562);
-    b = hh(b, c, d, a, x[i+14], 23, -35309556);
-    a = hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
-    d = hh(d, a, b, c, x[i+ 4], 11,  1272893353);
-    c = hh(c, d, a, b, x[i+ 7], 16, -155497632);
-    b = hh(b, c, d, a, x[i+10], 23, -1094730640);
-    a = hh(a, b, c, d, x[i+13], 4 ,  681279174);
-    d = hh(d, a, b, c, x[i+ 0], 11, -358537222);
-    c = hh(c, d, a, b, x[i+ 3], 16, -722521979);
-    b = hh(b, c, d, a, x[i+ 6], 23,  76029189);
-    a = hh(a, b, c, d, x[i+ 9], 4 , -640364487);
-    d = hh(d, a, b, c, x[i+12], 11, -421815835);
-    c = hh(c, d, a, b, x[i+15], 16,  530742520);
-    b = hh(b, c, d, a, x[i+ 2], 23, -995338651);
-
-    a = ii(a, b, c, d, x[i+ 0], 6 , -198630844);
-    d = ii(d, a, b, c, x[i+ 7], 10,  1126891415);
-    c = ii(c, d, a, b, x[i+14], 15, -1416354905);
-    b = ii(b, c, d, a, x[i+ 5], 21, -57434055);
-    a = ii(a, b, c, d, x[i+12], 6 ,  1700485571);
-    d = ii(d, a, b, c, x[i+ 3], 10, -1894986606);
-    c = ii(c, d, a, b, x[i+10], 15, -1051523);
-    b = ii(b, c, d, a, x[i+ 1], 21, -2054922799);
-    a = ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
-    d = ii(d, a, b, c, x[i+15], 10, -30611744);
-    c = ii(c, d, a, b, x[i+ 6], 15, -1560198380);
-    b = ii(b, c, d, a, x[i+13], 21,  1309151649);
-    a = ii(a, b, c, d, x[i+ 4], 6 , -145523070);
-    d = ii(d, a, b, c, x[i+11], 10, -1120210379);
-    c = ii(c, d, a, b, x[i+ 2], 15,  718787259);
-    b = ii(b, c, d, a, x[i+ 9], 21, -343485551);
-
-    a = add(a, olda);
-    b = add(b, oldb);
-    c = add(c, oldc);
-    d = add(d, oldd);
+    str += hex_tab.charAt((binarray[i>>2] >> ((i%4)*8+4)) & 0xF) +
+           hex_tab.charAt((binarray[i>>2] >> ((i%4)*8  )) & 0xF);
   }
-  return rhex(a) + rhex(b) + rhex(c) + rhex(d);
+  return str;
+}
+
+/*
+ * Convert an array of little-endian words to a base-64 string
+ */
+function binl2b64(binarray)
+{
+  var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  var str = "";
+  for(var i = 0; i < binarray.length * 4; i += 3)
+  {
+    var triplet = (((binarray[i   >> 2] >> 8 * ( i   %4)) & 0xFF) << 16)
+                | (((binarray[i+1 >> 2] >> 8 * ((i+1)%4)) & 0xFF) << 8 )
+                |  ((binarray[i+2 >> 2] >> 8 * ((i+2)%4)) & 0xFF);
+    for(var j = 0; j < 4; j++)
+    {
+      if(i * 8 + j * 6 > binarray.length * 32) str += b64pad;
+      else str += tab.charAt((triplet >> 6*(3-j)) & 0x3F);
+    }
+  }
+  return str;
 }
 EOS
 
@@ -356,7 +444,8 @@ This is a complete, runnable, tested program.
       'Password: ',
       $q -> password_field({name => 'my_password', size => 50}),
       $q -> br(),
-      $q -> submit({onClick => 'return RetMD5()'}),
+      'Generate str2hex_md5: ',
+      $q -> submit({onClick => 'return str2hex_md5()'}),
       $q -> end_form(),
       $q -> end_html();
 
@@ -406,10 +495,25 @@ To disable this module, simply use a submit button of the form:
 
 	$q -> submit();
 
-To enable the module, use a submit button with an onClick event handler that calls the
-Javascript function which converts your CGI password field's value into the MD5 digest of that value:
+To enable the module, use a submit button with an onClick event handler that calls a
+Javascript function which converts your CGI password field's value into the MD5 digest
+of that value. Use one of:
 
-	$q -> submit({onClick => 'return RetMD5()'});
+=over 4
+
+=item $q -> submit({onClick => 'return str2hex_md5()'});
+
+Returns a string of length 32.
+
+=item $q -> submit({onClick => 'return str2b64_md5()'});
+
+Returns a string of length 22.
+
+=item $q -> submit({onClick => 'return str2str_md5()'});
+
+Return a string of length 16 (approx :-).
+
+=back
 
 =head1 Example code
 
@@ -420,6 +524,10 @@ There is 1 demo file, md5.cgi.
 This is a CGI script identical to the one in the synopsis.
 
 Edit it to suit your circumstances.
+
+=head1 Related Modules
+
+Javascript::SHA1.
 
 =head1 Required Modules
 
